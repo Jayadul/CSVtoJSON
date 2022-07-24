8dc9b0d8d871ebfd8dfd4c522561ad28b7b32f92
shuvo <jayadul2@gmail.com>
2022-07-25 05:57:45 +0600

    summary added

--- src/Application/services/Transformation.cs
+++ src/Application/services/Transformation.cs
@@ -2,6 +2,11 @@
 {
     public static class Transformation
     {
+        /// <summary>
+        /// Replace space with underscore in string value
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
         public static string ToSankeCase(string str) => str.Trim().Replace(" ", "_");    
     }
 }
